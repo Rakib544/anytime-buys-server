58dd40514280d271ccb3bcef13854b8eb7ec7506
Rakib544 <md.rakib10122003@gmail.com>
2021-03-31 16:09:38 +0600

    almost server code done

--- index.js
+++ index.js
@@ -2,8 +2,12 @@ const express = require('express');
 const cors = require('cors');
 const app = express();
 const MongoClient = require('mongodb').MongoClient;
-app.use(express.json());
+const ObjectId = require('mongodb').ObjectID;
+const { ObjectID } = require('bson');
 app.use(cors());
+app.use(express.json());
+
+// app.use(express.urlencoded({ extended: false }))
 require('dotenv').config()
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.acxxo.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`;
@@ -13,20 +17,53 @@ app.get('/', (req, res) => {
     res.send('Hello I am working with my e-commerce project');
 })
 
-
-
-
 client.connect(err => {
-  const productsCollection = client.db(`${process.env.DB_NAME}`).collection("products");
-  
-  app.post('/addProduct', (req, res) => {
-      const productDetails = req.body;
-      productsCollection.insertOne(productDetails)
-      .then(result => {
-          console.log(result)
-          res.send(result.insertedCount > 0)
-      })
-  })
+    const productsCollection = client.db(`${process.env.DB_NAME}`).collection("products");
+    const orderCollection = client.db(`${process.env.DB_NAME}`).collection("orders");
+
+    //handling add product from admin panel functionality
+    app.post('/addProduct', (req, res) => {
+        const productDetails = req.body;
+        productsCollection.insertOne(productDetails)
+            .then(result => {
+                res.send(result.insertedCount > 0)
+            })
+    })
+
+    //handling load all products from database functionality
+    app.get('/allProducts', (req, res) => {
+        productsCollection.find({})
+            .toArray((err, products) => {
+                res.send(products)
+            })
+    })
+
+    //handling delete product from admin page functionality
+    app.delete('/deleteProduct', (req, res) => {
+        res.header("Access-Control-Allow-Origin", "*");
+        productsCollection.deleteOne({_id: ObjectID(req.body.id)})
+        .then(result => {
+            res.send(result.deletedCount > 0)
+        })
+    })
+
+    //handling checkout item functionality
+    app.get('/checkout/:id', (req, res) => {
+        const productId = {id: req.params.id};
+        productsCollection.find({_id: ObjectID(productId.id)})
+        .toArray((err, product) => {
+            res.send(product[0])
+        })
+    })
+
+    //handling order item functionality
+    app.post('/addOrder', (req, res) => {
+        orderCollection.insertOne(req.body)
+        .then(result => {
+            res.send(result.insertedCount > 0)
+        })
+    })
+    
 });
 
 
